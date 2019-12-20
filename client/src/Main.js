fd0d18b92ec98e4d44ef86476d22a853ad4283da
kxkaro <kar.krajewska@gmail.com>
2019-12-20 19:57:21 +0100

    switch between pages and get initial data setup

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,32 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
+import sprintsService from './services/sprintsService';
 
-class Main extends React.Component {
+function Main(props) {
+    const [sprints, setSprints] = useState(null);
 
-    constructor(props) {
-        super(props);
+    const getSprints = async () => {
+        let res = await sprintsService.getAll();
+        console.log(res);
+        setSprints(res);
     }
 
-    render() {
-        return (
-            <div>
-                <div className="landing-header">
-                    <h1>
-                        <span className="landing-header-main">
-                            This is the main page
-                        </span>
-                    </h1>
-                </div>
-                <ul className="slideshow">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                
-            </div>
-        );
-    }
+    useEffect(() => {
+        if (!sprints) {
+            getSprints();
+        }
+    })
+
+    return (
+        <div>
+            <ul>
+                {(sprints && sprints.length > 0) ? (
+                    sprints.map(sprint => (
+                        <li key={sprint._id} style={{ color: "#FFFFFF" }}>
+                            <h3>{sprint.name}</h3>
+                            <p>{sprint.description}</p>
+                        </li>
+                    ))
+                ) : (
+                        <p>No sprints found</p>
+                    )}
+            </ul>
+            <a
+                onClick={() => props.onClick("landing")}
+                className="ui inverted button btn-slideshow"
+            >
+                GO BACK!
+            </a>
+        </div>
+    );
+
 }
 
 
--- client/src/Page.js
+++ client/src/Page.js
@@ -9,9 +9,26 @@ class Page extends React.Component {
     }
 
     render() {
-        
+        const pages = [
+            {
+                id: 0,
+                name: "landing",
+                html: <Landing onClick={() => this.props.onClick("main")} />
+            },
+            {
+                id: 1,
+                name: "main",
+                html: <Main onClick={() => this.props.onClick("landing")} />
+            }
+        ];
+
         return (
-            this.props.page === "landing" ? <Landing onClick={this.props.onClick} /> : <Main />
+            <div>
+                {pages.filter(page => (
+                    page.name === this.props.page
+                ))[0].html};
+            </div>
+            // this.props.page === 0 ? <Landing onClick={() => this.props.onClick(1)} /> : <Main />
         );
     }
 }
