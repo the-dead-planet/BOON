8915b4309adc562294d6d19bab261c7fdac81d5b
tomic <tomic@onet.com.pl>
2019-12-23 12:07:25 +0100

    Redirecting page decorators

---
+++ client/src/components/authenticatedPage.js
@@ -0,0 +1,7 @@
+import { withLoginRequired } from '../utils/authDecorators';
+import { withRouter } from 'react-router';
+
+// HOC for pages requiring the user to be authenticated.
+//
+// Redirects to the login page if the user is not logged in.
+export const authenticatedPage = wrappedComponent => withRouter(withLoginRequired(wrappedComponent));
