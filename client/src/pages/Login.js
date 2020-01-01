bf1cad85c12d2a898c0035d49cdea9ef9fe1367e
tomic <tomic@onet.com.pl>
2020-01-01 19:29:43 +0100

    Fix post-login user data unpacking bug

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,17 +4,17 @@ import NavBar from '../components/NavBar';
 import authService from '../services/authService';
 import { interceptPage } from '../components/interceptPage';
 
-const Login = ({next, onLoginSuccess, user }) => (
+const Login = ({ next, onLoginSuccess, user }) => (
     <div>
         <NavBar user={user} />
         <h1>Login</h1>
         <AuthForm
-            type='login'
+            type="login"
             onSubmit={({ password, email }) => {
                 authService
-                    .login( password, email)
-                    .then(resp => {
-                        onLoginSuccess(resp);
+                    .login(password, email)
+                    .then(({ user }) => {
+                        onLoginSuccess(user);
                         next();
                     })
                     .catch(err => {
