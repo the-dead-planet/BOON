34e85a4106873afda78528f32f4fa16761055b84
tomic <tomic@onet.com.pl>
2020-01-03 22:22:46 +0100

    Link to sprint detail page from preview

--- client/src/components/sprint/Preview.js
+++ client/src/components/sprint/Preview.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 
 export const Preview = ({ _id, number, name, dateFrom, dateTo, description }) => (
-    <div key={_id} style={{ color: '#000' }}>
-        <Typography variant="h5" component="h3">
-            {number} : {name}
-        </Typography>
-        <Typography component="p">
-            {dateFrom.slice(0, 10)} - {dateTo.slice(0, 10)}
-        </Typography>
-        <Typography component="p">{description}</Typography>
-    </div>
+    <Link to={`/sprints/${_id}`}>
+        <div key={_id} style={{ color: '#000' }}>
+            <Typography variant="h5" component="h3">
+                {number} : {name}
+            </Typography>
+            <Typography component="p">
+                {dateFrom.slice(0, 10)} - {dateTo.slice(0, 10)}
+            </Typography>
+            <Typography component="p">{description}</Typography>
+        </div>
+    </Link>
 );
