8f89305f27efa880a222641d6e44f545b5ebc782
kxkaro <kar.krajewska@gmail.com>
2020-03-14 13:00:23 +0100

    repair comment collapse panels in post list

--- client/src/components/sprint/details/PostsList.js
+++ client/src/components/sprint/details/PostsList.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import List from '@material-ui/core/List';
-import { Box } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import moment from 'moment';
 import { EXT_DATE_FORMAT } from '../../../utils/constants';
 import { ObjectDeleteButton } from '../../Buttons';
 import { Comments } from '../../Comments';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -20,7 +17,6 @@ import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import ModeCommentOutlinedIcon from '@material-ui/icons/ModeCommentOutlined';
 
@@ -56,57 +52,59 @@ const useStyles = makeStyles(theme => ({
 export const PostsList = ({ user, posts, push }) => {
     const classes = useStyles();
 
-    const [expanded, setExpanded] = React.useState(false);
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
+    const [expanded, setExpanded] = React.useState([]);
+    const handleExpandClick = i => {
+        setExpanded(posts.map((post, index) => (index === i ? !expanded[index] : expanded[index])));
     };
 
     return (
         <List>
             {(posts || []).map((post, index) => (
-                <Box key={index}>
-                    <Card className={classes.root}>
-                        <CardHeader
-                            avatar={null}
-                            action={
-                                <IconButton aria-label="settings">
-                                    <MoreVertIcon />
-                                </IconButton>
-                            }
-                            title={post.title}
-                            subheader={`${post.author.username} / ${moment(post.created).format(EXT_DATE_FORMAT)}`}
-                        />
-                        {/* <CardMedia
+                <Card key={post._id} className={classes.root}>
+                    <CardHeader
+                        avatar={null}
+                        action={
+                            <IconButton aria-label="settings">
+                                <MoreVertIcon />
+                            </IconButton>
+                        }
+                        title={post.title}
+                        subheader={`${post.author.username} / ${moment(post.created).format(EXT_DATE_FORMAT)}`}
+                    />
+                    {/* <CardMedia
                             className={classes.media}
                             image="/static/images/cards/paella.jpg"
                             title="Paella dish"
                         /> */}
-                        <CardContent>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                {post.body}
-                            </Typography>
-                        </CardContent>
-                        <CardActions disableSpacing>
-                            <IconButton aria-label="add to favorites">
-                                <FavoriteIcon />
-                            </IconButton>
-                            {/* <IconButton aria-label="share">
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {post.body}
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                        </IconButton>
+                        {/* <IconButton aria-label="share">
                                 <ShareIcon />
                             </IconButton> */}
-                            <IconButton onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more">
-                                <ModeCommentOutlinedIcon />
-                                <Typography variant="caption">{post.comments.length}</Typography>
-                            </IconButton>
+                        <IconButton
+                            onClick={() => handleExpandClick(index)}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ModeCommentOutlinedIcon />
+                            <Typography variant="caption">{post.comments.length}</Typography>
+                        </IconButton>
 
-                            <IconButton className={classes.right}>
-                                <ObjectDeleteButton user={user} model="Post" object={post} />
-                            </IconButton>
-                        </CardActions>
-                        <CardContent>
-                            <Comments expanded={expanded} user={user} model="Post" {...post} />
-                        </CardContent>
-                    </Card>
-                </Box>
+                        <IconButton className={classes.right}>
+                            <ObjectDeleteButton user={user} model="Post" object={post} />
+                        </IconButton>
+                    </CardActions>
+                    <CardContent>
+                        <Comments expanded={expanded[index] || false} user={user} model="Post" {...post} />
+                    </CardContent>
+                </Card>
             ))}
         </List>
     );
