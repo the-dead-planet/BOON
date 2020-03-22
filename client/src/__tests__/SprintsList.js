661d8d721219421698291573bdba24cfa2726dc1
tomic <tomic@onet.com.pl>
2020-03-22 21:51:11 +0100

    Cleanup mock usage in client tests; add a Login page test

--- client/src/__tests__/SprintsList.js
+++ client/src/__tests__/SprintsList.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
-import authService, { resolveWhoAmi } from '../services/authService';
-import MockSprintsService from '../services/sprintsService';
+import authService from '../services/authService';
+import sprintsService from '../services/sprintsService';
 
 jest.mock('../services/authService');
 jest.mock('../services/sprintsService');
 
 describe('app', () => {
     test('Displays a list of sprints', async () => {
-        const { getByText, findByText, findAllByText } = render(<App />);
-
-        // Resolve all requests upfront to make the test simpler.
-        resolveWhoAmi({ user: { username: 'username' } });
-        MockSprintsService.getAll.mockResolvedValue([
+        authService.whoami.mockResolvedValue({ user: { username: 'username' } });
+        sprintsService.getAll.mockResolvedValue([
             { _id: 'sprint0Id', number: 0, title: 'sprint0Title', author: { _id: 'user0Id' }, likes: [], comments: [] },
         ]);
+
+        const { getByText, findByText, findAllByText } = render(<App />);
 
         const enterButton = await findByText(/enter the boon/i);
         fireEvent.click(enterButton);
