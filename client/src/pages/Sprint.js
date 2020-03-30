0a3afce0e940a6c593e51f516429af045845d2b7
tomic <tomic@onet.com.pl>
2020-03-30 08:58:45 +0200

    Fix failing tests, refresh sprints from DB

--- client/src/pages/Sprint.js
+++ client/src/pages/Sprint.js
@@ -8,27 +8,45 @@ import withShowError from '../components/withShowError';
 // If path is /sprints, redirect to the newest sprint
 const Sprint = ({ user, sprints, setSprints, notificationsProps, onError, showError }) => {
     const { id } = useParams();
-    let _id = id;
-    _id = !id && !_id && sprints ? [...sprints].sort((a, b) => new Date(b.dateTo) - new Date(a.dateTo))[0]._id : null;
+
+    let sprintToDisplayId = id;
+    // If no specific `Sprint` has been specified, try to redirect to the
+    // detail page of the most recent sprint.
+    if (sprintToDisplayId == undefined) {
+        // Wait for the sprints to load.
+        // TODO: consider adding a HOC waiting for pending requests and rendering a spinner.
+        if (sprints) {
+            // If no sprints exists, there's nowhere to redirect to.
+            // TODO: consider handling this case by rendering a message.
+            if (sprints.length !== 0) {
+                const mostRecentSprint = sprints.reduce((a, b) => (new Date(a.dateTo) > new Date(b.dateTo) ? a : b));
+                sprintToDisplayId = mostRecentSprint._id;
+            }
+        }
+    }
 
     const getSprints = async () => {
         let res = await sprintsService.getAll().catch(onError);
         await setSprints(res);
     };
 
+    // Fetch sprints on the first render.
+    // It will send a request when the user re-enters the sprints list page from some other page (e.g. form).
+    // This way, the user has a way of refreshing sprints data.
     useEffect(() => {
-        if (!sprints) {
-            getSprints();
-        }
-    });
+        getSprints();
+    }, []);
 
-    return id ? (
+    return sprintToDisplayId && sprintToDisplayId !== id ? (
+        <Redirect to={`/sprints/${sprintToDisplayId}`} />
+    ) : (
         <AppLayout user={user} {...notificationsProps}>
-            <SprintView user={user} sprints={sprints} sprintId={id} onError={onError} showError={showError} />
+            {/* Render the layout even if no sprint can be shown. The user would see a blank screen otherwise. */}
+            {sprintToDisplayId && (
+                <SprintView user={user} sprints={sprints} sprintId={id} onError={onError} showError={showError} />
+            )}
         </AppLayout>
-    ) : _id ? (
-        <Redirect to={`/sprints/${_id}`} />
-    ) : null;
+    );
 };
 
 export default withShowError(Sprint);
