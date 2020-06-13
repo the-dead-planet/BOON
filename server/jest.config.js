38e3540b13d9aa5f5b59c94520da5b964766dfe5
tomic <tomic@onet.com.pl>
2020-06-13 12:52:01 +0200

    Clean up project structure and configs

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     preset: '@shelf/jest-mongodb',
-    setupFilesAfterEnv: ['./src/setupTests.js'],
+    setupFilesAfterEnv: ['./setupTests.js'],
+    testEnvironment: 'node',
 };
