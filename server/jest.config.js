9bd106eb74c1de92afa33a8982a786cd0456b622
tomic <tomic@onet.com.pl>
2020-06-13 13:29:33 +0200

    Enable JS -> TS transpilation of server/src/ code

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.json',
+        },
+    },
+    moduleFileExtensions: ['ts', 'js'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    testMatch: [
+        '**/src/**/*.test.*', // Match files under src/ and let the test framework transpile them.
+    ],
     preset: '@shelf/jest-mongodb',
     setupFilesAfterEnv: ['./setupTests.js'],
     testEnvironment: 'node',
