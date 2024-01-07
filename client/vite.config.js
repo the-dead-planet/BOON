d16b1986e8bda04ec46f33c931bbd58ee75fa298
spookydoodle <kxkaro@gmail.com>
2024-01-07 16:54:11 +0100

    Update vite plugins

--- client/vite.config.js
+++ client/vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
         },
         resolve: {
             alias: {
-                '@': normalizePath(path.resolve(__dirname, './src')),
+                '@': normalizePath(path.resolve(__dirname, './src')), // normalize for Windows
             },
         },
         server: {
